71da6f6c00f8b28b6955ce64b38cea7c70d805e9
Mostafa Nady <mustafa2nady@gmail.com>
2025-02-19 04:13:42 +0200

    Time: 0 ms (100%), Space: 9 MB (77.25%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,51 +1,56 @@
 class Solution {
 public:
-    set<string> st;
-    void g(int& n, string s, int idx) {
+    string ans = "";
+    int i = 0;
+    void g(int& n, string s, int idx, int k) {
         if (idx == n && s.length() == n) {
-            st.insert(s);
+            i++;
+            if (i == k) {
+                ans = s;
+            }
             return;
         }
-
+        if (!ans.empty() || i == k) {
+            return;
+        }
         if (!s.empty() && s.back() != 'a') {
             s += 'a';
-            g(n, s, idx + 1);
+            g(n, s, idx + 1, k);
             s.pop_back();
         } else {
             if (s.empty()) {
                 s += 'a';
-                g(n, s, idx + 1);
+                g(n, s, idx + 1, k);
                 s.pop_back();
             }
         }
 
         if (!s.empty() && s.back() != 'b') {
             s += 'b';
-            g(n, s, idx + 1);
+            g(n, s, idx + 1, k);
             s.pop_back();
         } else {
             if (s.empty()) {
                 s += 'b';
-                g(n, s, idx + 1);
+                g(n, s, idx + 1, k);
                 s.pop_back();
             }
         }
         if (!s.empty() && s.back() != 'c') {
             s += 'c';
-            g(n, s, idx + 1);
+            g(n, s, idx + 1, k);
             s.pop_back();
         } else {
             if (s.empty()) {
                 s += 'c';
-                g(n, s, idx + 1);
+                g(n, s, idx + 1, k);
                 s.pop_back();
             }
         }
     }
     string getHappyString(int& n, int& k) {
-        ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-        g(n, "", 0);
-        vector<string> vec(st.begin(), st.end());
-        return (k > vec.size()) ? "" : vec[k - 1];
+        g(n, "", 0, k);
+        this->i = 0;
+        return ans;
     }
 };
