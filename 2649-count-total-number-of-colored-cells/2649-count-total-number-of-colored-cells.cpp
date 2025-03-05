737b8a170fa2734c4b8ed8384efa49449774a804
MostafaNady2 <mustafa2nady@gmail.com>
2025-03-05 07:11:35 +0200

    Time: 0 ms (100%), Space: 8 MB (7.1%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -7,9 +7,15 @@ public:
         }else if(n==2){
             return 5;
         }else{
-            for(int i = 3 ; i <= n;i++){
-                ans+=(2*i - 2)*2;
-            }
+            // for(int i = 3 ; i <= n;i++){
+            //     ans+=(2*i - 2)*2;
+            // }
+
+            long long term =(3+n);
+            term*=(n-2);
+            term*=2;
+            ans+=term;
+            ans-=((n-2)*4);
         }
         return ans;
     }
