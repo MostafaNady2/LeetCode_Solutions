35538c3a42689aa00221289da9f733ed1ea1a81a
MostafaNady2 <mustafa2nady@gmail.com>
2025-04-17 22:10:28 +0200

    Time: 3 ms (29.7%), Space: 15.4 MB (91.09%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -4,12 +4,12 @@ public:
         int n = nums.size();
         int ans = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if ((i * j) % k == 0 && nums[i] == nums[j] && i != j) {
+            for (int j = i+1; j < n; j++) {
+                if ((i * j) % k == 0 && nums[i] == nums[j]) {
                     ans++;
                 }
             }
         }
-        return ans/2;
+        return ans;
     }
 };
