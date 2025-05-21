44f55efea9d822b417a5a173ecd2d914e02b249f
Mostafa Nady Sadeq Mahmoud <mustafa2nady@gmail.com>
2025-05-21 07:14:57 +0300

    Time: 9 ms (54.9%), Space: 292.8 MB (86.84%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -3,19 +3,21 @@ public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         const int n = nums.size();
         int m = queries.size();
-        vector<int> arr(n+1,0);
+        vector<int> arr(n + 1, 0);
         for (int i = 0; i < m; i++) {
             arr[queries[i][0]]--;
             arr[queries[i][1] + 1]++;
         }
+        if(arr[0]+nums[0] > 0){
+            return false;
+        }
         for (int i = 1; i < n; i++) {
             arr[i] += arr[i - 1];
-        }
-        for (int i = 0; i < n; i++) {
             if (arr[i] + nums[i] > 0) {
                 return false;
             }
         }
+
         return true;
     }
 };
